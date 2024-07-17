ef62b0e14b16d429ec876f77821fa0b271afa972
srsviegas <rudiero.viegas@gmail.com>
2024-07-17 12:45:13 -0300

    ADD: Laboratório 1 (OpenGL)

---
+++ lab1-opengl/entrega/main-fan.cpp
@@ -0,0 +1,415 @@
+//     Universidade Federal do Rio Grande do Sul
+//             Instituto de Informática
+//       Departamento de Informática Aplicada
+//
+//    INF01047 Fundamentos de Computação Gráfica
+//               Prof. Eduardo Gastal
+//
+//                   LABORATÓRIO 1
+//
+
+#define _USE_MATH_DEFINES
+
+// Arquivos "headers" padrões de C podem ser incluídos em um
+// programa C++, sendo necessário somente adicionar o caractere
+// "c" antes de seu nome, e remover o sufixo ".h". Exemplo:
+//    #include <stdio.h> // Em C
+//  vira
+//    #include <cstdio> // Em C++
+//
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+// Headers abaixo são específicos de C++
+#include <string>
+#include <fstream>
+#include <sstream>
+
+// Headers das bibliotecas OpenGL
+#include <glad/glad.h>  // Criação de contexto OpenGL 3.3
+#include <GLFW/glfw3.h> // Criação de janelas do sistema operacional
+
+// Headers locais, definidos na pasta "include/"
+#include "utils.h"
+
+// Declaração de várias funções utilizadas em main().  Essas estão definidas
+// logo após a definição de main() neste arquivo.
+GLuint BuildTriangles();                                                     // Constrói triângulos para renderização
+void LoadShadersFromFiles();                                                 // Carrega os shaders de vértice e fragmento, criando um programa de GPU
+GLuint LoadShader_Vertex(const char *filename);                              // Carrega um vertex shader
+GLuint LoadShader_Fragment(const char *filename);                            // Carrega um fragment shader
+void LoadShader(const char *filename, GLuint shader_id);                     // Função utilizada pelas duas acima
+GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id); // Cria um programa de GPU
+
+// Funções callback para comunicação com o sistema operacional e interação do
+// usuário. Veja mais comentários nas definições das mesmas, abaixo.
+void FramebufferSizeCallback(GLFWwindow *window, int width, int height);
+void ErrorCallback(int error, const char *description);
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mod);
+
+GLuint g_GpuProgramID = 0;
+
+const int vertexCount = 17;
+const int coefficientCount = vertexCount * 4;
+const int polygonSides = vertexCount - 1;
+
+int main()
+{
+    int success = glfwInit();
+    if (!success)
+    {
+        fprintf(stderr, "ERROR: glfwInit() failed.\n");
+        std::exit(EXIT_FAILURE);
+    }
+
+    glfwSetErrorCallback(ErrorCallback);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window;
+    window = glfwCreateWindow(500, 500, "INF01047 - 00342978 - Sandro Rudiero Saibro Viégas", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        fprintf(stderr, "ERROR: glfwCreateWindow() failed.\n");
+        std::exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *glversion = glGetString(GL_VERSION);
+    const GLubyte *glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    printf("GPU: %s, %s, OpenGL %s, GLSL %s\n", vendor, renderer, glversion, glslversion);
+
+    LoadShadersFromFiles();
+
+    GLuint vertex_array_object_id = BuildTriangles();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(g_GpuProgramID);
+
+        glBindVertexArray(vertex_array_object_id);
+        glDrawElements(GL_TRIANGLE_FAN, 18, GL_UNSIGNED_BYTE, 0);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+// Constrói triângulos para futura renderização
+GLuint BuildTriangles()
+{
+    GLfloat NDC_coefficients[coefficientCount] = {0.0f, 0.0f, 0.0f, 1.0f};
+    for (int i = 0; i < vertexCount; i++)
+    {
+        GLfloat angle = 2.0f * M_PI * i / polygonSides;
+        GLfloat x = 0.7f * cos(angle);
+        GLfloat y = 0.7f * sin(angle);
+
+        NDC_coefficients[4 * (i + 1) + 0] = x;
+        NDC_coefficients[4 * (i + 1) + 1] = y;
+        NDC_coefficients[4 * (i + 1) + 2] = 0.0f;
+        NDC_coefficients[4 * (i + 1) + 3] = 1.0f;
+    }
+
+    GLuint VBO_NDC_coefficients_id;
+    glGenBuffers(1, &VBO_NDC_coefficients_id);
+
+    GLuint vertex_array_object_id;
+    glGenVertexArrays(1, &vertex_array_object_id);
+
+    glBindVertexArray(vertex_array_object_id);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_NDC_coefficients_id);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(NDC_coefficients), NULL, GL_STATIC_DRAW);
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(NDC_coefficients), NDC_coefficients);
+
+    GLuint location = 0;            // "(location = 0)" em "shader_vertex.glsl"
+    GLint number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+    glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glEnableVertexAttribArray(location);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLfloat color_coefficients[coefficientCount] = {1.0f, 0.0f, 0.0f, 1.0f};
+    for (int i = 1; i < vertexCount; i++)
+    {
+        color_coefficients[4 * i + 2] = 1.0f;
+        color_coefficients[4 * i + 3] = 1.0f;
+    }
+    GLuint VBO_color_coefficients_id;
+    glGenBuffers(1, &VBO_color_coefficients_id);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_color_coefficients_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color_coefficients), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(color_coefficients), color_coefficients);
+    location = 1;             // "(location = 1)" em "shader_vertex.glsl"
+    number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+    glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(location);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLubyte indices[vertexCount + 1] = {0};
+    for (int i = 0; i < vertexCount; i++)
+    {
+        indices[i] = i;
+    }
+    indices[vertexCount] = 1;
+
+    GLuint indices_id;
+    glGenBuffers(1, &indices_id);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_id);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), NULL, GL_STATIC_DRAW);
+
+    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(indices), indices);
+
+    glBindVertexArray(0);
+
+    return vertex_array_object_id;
+}
+
+// Carrega um Vertex Shader de um arquivo GLSL. Veja definição de LoadShader() abaixo.
+GLuint LoadShader_Vertex(const char *filename)
+{
+    // Criamos um identificador (ID) para este shader, informando que o mesmo
+    // será aplicado nos vértices.
+    GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+
+    // Carregamos e compilamos o shader
+    LoadShader(filename, vertex_shader_id);
+
+    // Retorna o ID gerado acima
+    return vertex_shader_id;
+}
+
+// Carrega um Fragment Shader de um arquivo GLSL . Veja definição de LoadShader() abaixo.
+GLuint LoadShader_Fragment(const char *filename)
+{
+    // Criamos um identificador (ID) para este shader, informando que o mesmo
+    // será aplicado nos fragmentos.
+    GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Carregamos e compilamos o shader
+    LoadShader(filename, fragment_shader_id);
+
+    // Retorna o ID gerado acima
+    return fragment_shader_id;
+}
+
+// Função auxilar, utilizada pelas duas funções acima. Carrega código de GPU de
+// um arquivo GLSL e faz sua compilação.
+void LoadShader(const char *filename, GLuint shader_id)
+{
+    // Lemos o arquivo de texto indicado pela variável "filename"
+    // e colocamos seu conteúdo em memória, apontado pela variável
+    // "shader_string".
+    std::ifstream file;
+    try
+    {
+        file.exceptions(std::ifstream::failbit);
+        file.open(filename);
+    }
+    catch (std::exception &e)
+    {
+        fprintf(stderr, "ERROR: Cannot open file \"%s\".\n", filename);
+        std::exit(EXIT_FAILURE);
+    }
+    std::stringstream shader;
+    shader << file.rdbuf();
+    std::string str = shader.str();
+    const GLchar *shader_string = str.c_str();
+    const GLint shader_string_length = static_cast<GLint>(str.length());
+
+    // Define o código do shader GLSL, contido na string "shader_string"
+    glShaderSource(shader_id, 1, &shader_string, &shader_string_length);
+
+    // Compila o código do shader GLSL (em tempo de execução)
+    glCompileShader(shader_id);
+
+    // Verificamos se ocorreu algum erro ou "warning" durante a compilação
+    GLint compiled_ok;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &compiled_ok);
+
+    GLint log_length = 0;
+    glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &log_length);
+
+    // Alocamos memória para guardar o log de compilação.
+    // A chamada "new" em C++ é equivalente ao "malloc()" do C.
+    GLchar *log = new GLchar[log_length];
+    glGetShaderInfoLog(shader_id, log_length, &log_length, log);
+
+    // Imprime no terminal qualquer erro ou "warning" de compilação
+    if (log_length != 0)
+    {
+        std::string output;
+
+        if (!compiled_ok)
+        {
+            output += "ERROR: OpenGL compilation of \"";
+            output += filename;
+            output += "\" failed.\n";
+            output += "== Start of compilation log\n";
+            output += log;
+            output += "== End of compilation log\n";
+        }
+        else
+        {
+            output += "WARNING: OpenGL compilation of \"";
+            output += filename;
+            output += "\".\n";
+            output += "== Start of compilation log\n";
+            output += log;
+            output += "== End of compilation log\n";
+        }
+
+        fprintf(stderr, "%s", output.c_str());
+    }
+
+    // A chamada "delete" em C++ é equivalente ao "free()" do C
+    delete[] log;
+}
+
+// Função que carrega os shaders de vértices e de fragmentos que serão
+// utilizados para renderização. Veja slides 180-200 do documento Aula_03_Rendering_Pipeline_Grafico.pdf.
+//
+void LoadShadersFromFiles()
+{
+    // Note que o caminho para os arquivos "shader_vertex.glsl" e
+    // "shader_fragment.glsl" estão fixados, sendo que assumimos a existência
+    // da seguinte estrutura no sistema de arquivos:
+    //
+    //    + FCG_Lab_01/
+    //    |
+    //    +--+ bin/
+    //    |  |
+    //    |  +--+ Release/  (ou Debug/ ou Linux/)
+    //    |     |
+    //    |     o-- main.exe
+    //    |
+    //    +--+ src/
+    //       |
+    //       o-- shader_vertex.glsl
+    //       |
+    //       o-- shader_fragment.glsl
+    //
+    GLuint vertex_shader_id = LoadShader_Vertex("../../src/shader_vertex.glsl");
+    GLuint fragment_shader_id = LoadShader_Fragment("../../src/shader_fragment.glsl");
+
+    // Deletamos o programa de GPU anterior, caso ele exista.
+    if (g_GpuProgramID != 0)
+        glDeleteProgram(g_GpuProgramID);
+
+    // Criamos um programa de GPU utilizando os shaders carregados acima.
+    g_GpuProgramID = CreateGpuProgram(vertex_shader_id, fragment_shader_id);
+}
+
+// Esta função cria um programa de GPU, o qual contém obrigatoriamente um
+// Vertex Shader e um Fragment Shader.
+GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id)
+{
+    // Criamos um identificador (ID) para este programa de GPU
+    GLuint program_id = glCreateProgram();
+
+    // Definição dos dois shaders GLSL que devem ser executados pelo programa
+    glAttachShader(program_id, vertex_shader_id);
+    glAttachShader(program_id, fragment_shader_id);
+
+    // Linkagem dos shaders acima ao programa
+    glLinkProgram(program_id);
+
+    // Verificamos se ocorreu algum erro durante a linkagem
+    GLint linked_ok = GL_FALSE;
+    glGetProgramiv(program_id, GL_LINK_STATUS, &linked_ok);
+
+    // Imprime no terminal qualquer erro de linkagem
+    if (linked_ok == GL_FALSE)
+    {
+        GLint log_length = 0;
+        glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &log_length);
+
+        // Alocamos memória para guardar o log de compilação.
+        // A chamada "new" em C++ é equivalente ao "malloc()" do C.
+        GLchar *log = new GLchar[log_length];
+
+        glGetProgramInfoLog(program_id, log_length, &log_length, log);
+
+        std::string output;
+
+        output += "ERROR: OpenGL linking of program failed.\n";
+        output += "== Start of link log\n";
+        output += log;
+        output += "== End of link log\n";
+
+        // A chamada "delete" em C++ é equivalente ao "free()" do C
+        delete[] log;
+
+        fprintf(stderr, "%s", output.c_str());
+    }
+
+    // Retorna o ID gerado acima
+    return program_id;
+}
+
+// Definição da função que será chamada sempre que a janela do sistema
+// operacional for redimensionada, por consequência alterando o tamanho do
+// "framebuffer" (região de memória onde são armazenados os pixels da imagem).
+void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    // Indicamos que queremos renderizar em toda região do framebuffer. A
+    // função "glViewport" define o mapeamento das "normalized device
+    // coordinates" (NDC) para "pixel coordinates".  Essa é a operação de
+    // "Screen Mapping" ou "Viewport Mapping" vista em aula (slides 141-148 do documento Aula_03_Rendering_Pipeline_Grafico.pdf).
+    glViewport(0, 0, width, height);
+}
+
+// Definição da função que será chamada sempre que o usuário pressionar alguma
+// tecla do teclado. Veja http://www.glfw.org/docs/latest/input_guide.html#input_key
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mod)
+{
+    // ====================
+    // Não modifique este loop! Ele é utilizando para correção automatizada dos
+    // laboratórios. Deve ser sempre o primeiro comando desta função KeyCallback().
+    for (int i = 0; i < 10; ++i)
+        if (key == GLFW_KEY_0 + i && action == GLFW_PRESS && mod == GLFW_MOD_SHIFT)
+            std::exit(100 + i);
+    // ====================
+
+    // Se o usuário pressionar a tecla ESC, fechamos a janela.
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+// Definimos o callback para impressão de erros da GLFW no terminal
+void ErrorCallback(int error, const char *description)
+{
+    fprintf(stderr, "ERROR: GLFW: %s\n", description);
+}
+
+// vim: set spell spelllang=pt_br :
---
+++ lab1-opengl/src/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/glad.c.obj"
+  "CMakeFiles/main.dir/src/glad.c.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "D:/UFRGS/05 Fundamentos de Computação Gráfica/Laboratorio 1/Laboratorio_1_Codigo_Fonte/bin/Debug/main.exe"
+  "D:/UFRGS/05 Fundamentos de Computação Gráfica/Laboratorio 1/Laboratorio_1_Codigo_Fonte/bin/Debug/main.exe.manifest"
+  "D:/UFRGS/05 Fundamentos de Computação Gráfica/Laboratorio 1/Laboratorio_1_Codigo_Fonte/bin/Debug/main.pdb"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
